887dd99bfdc53e1bbfcdd0f8383e94497ce009e0
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2023-10-21 15:32:11 +0300

    add calculator bot

---
+++ bot-calculator/go.mod
@@ -0,0 +1,5 @@
+module bot-calculator
+
+go 1.21.0
+
+require github.com/maja42/goval v1.3.1
---
+++ bot-calculator/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"botyard"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+
+	"github.com/maja42/goval"
+)
+
+const port = "4000"
+const botKey = "PASTE_BOT_KEY_HERE"
+
+func main() {
+	http.HandleFunc("/webhook", webhookHandler)
+
+	fmt.Println("Bot is running...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+var zeroDiv = regexp.MustCompile(`\d+\s*/\s*0`)
+
+func messageHandler(msg botyard.Message) {
+	reply := ""
+
+	switch msg.Body {
+	case "/start":
+		reply = "👋 Hi, I am a calculator bot and can calculate various maths expressions.\n\n"
+		reply += "Try sending me something and I'll try to solve it. For example:\n\n"
+		reply += "<b>2 + 2</b>\n\n"
+		reply += "<b>25 * 90</b>\n\n"
+		reply += "<b>(12 + 30) / 6 - 11</b>\n\n"
+		reply += "<b>22.0 / 7.01</b>\n\n"
+		reply += "<b>1 << 32</b>\n\n"
+	default:
+		if zeroDiv.MatchString(msg.Body) {
+			reply = "Divison by zero is not allowed."
+			break
+		}
+
+		eval := goval.NewEvaluator()
+		result, err := eval.Evaluate(msg.Body, nil, nil)
+		if err != nil {
+			reply = "Sorry, but I don't understand you."
+			break
+		}
+
+		reply = fmt.Sprintf("%v", result)
+	}
+
+	botyard.SendMessage(msg.ChatId, reply, nil, botKey)
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var msg botyard.Message
+
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	go messageHandler(msg)
+
+	w.WriteHeader(http.StatusOK)
+}
